3e1f0b16a9a351c7c146e56dac975cbc3f1fdb19
fries <149796114+friesuu@users.noreply.github.com>
2024-05-24 10:28:42 +0800

    Delete MLLAB1.py

--- MLLAB1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-url = ".\Machine Learning (WIA1006)\ModifiedCrabAgePrediction.csv"
-crab_df = pd.read_csv(url, header = 0)
-
-print(crab_df.info())
-print(len(crab_df))
